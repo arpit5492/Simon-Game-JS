31bc31ba33c56acaca898ba00e62ff2e3589a7e6
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-25 02:15:24 -0700

    Step 6 completed

--- script.js
+++ script.js
@@ -13,16 +13,45 @@ const nxtSeq = function(arr) {
     // audio.play();
 };
 
+nxtSeq(buttonColors);
+
 const makeSound = function(name){
     let audio = new Audio('/sounds/' + name + '.mp3');
     audio.play();
 };
 
-nxtSeq(buttonColors);
+// Sound happens when a button is clicked(Any of the below method will work)
 
-$(".btn").on("click", function() {
+// $(".btn").on("click", function() {
+//     const userChosenColor = $(this).attr("id");
+//     userClickedPattern.push(userChosenColor);
+//     // console.log(userClickedPattern);
+//     makeSound(userChosenColor);
+// });
+
+$(".btn").click(function(){
     const userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     // console.log(userClickedPattern);
     makeSound(userChosenColor);
+    animatePress(userChosenColor);
 });
+
+// Adding the pressed class(Any of the below method will work)
+
+// $(".btn").on("click", function(){
+//     let pressedColor = $(this);
+//     pressedColor.addClass("pressed");
+//     setTimeout(function(){
+//         pressedColor.removeClass("pressed");
+//     }, 100);
+// });
+
+const animatePress = function(currentColor){
+    let pressedColor = $("#"+currentColor);
+    pressedColor.addClass("pressed");
+    setTimeout(function(){
+        pressedColor.removeClass("pressed");
+    }, 100);
+};
+
